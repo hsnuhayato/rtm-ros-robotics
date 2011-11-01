1f9b0abd8ca736316dddf1f9fc966648fda8a2cd
rosen.diankov@gmail.com <rosen.diankov@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-11-01 13:32:16 +0000

    doc

---
+++ rst/updatetexmf.py
@@ -0,0 +1,4 @@
+#!/bin/env python
+s=open('/etc/texmf/texmf.cnf','r').read()
+if not s.startswith('CMAPFONTS'):
+    open('/etc/texmf/texmf.cnf','w').write('CMAPFONTS = .;\$TEXMF/fonts/cmap//;/usr/share/fonts/cmap//\n'+s)
