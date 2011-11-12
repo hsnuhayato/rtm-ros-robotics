b9d7a82f4782c2611370465128dc946442addb8a
rtmrosrobotics.testing@gmail.com <rtmrosrobotics.testing@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-11-12 13:34:14 +0000

    Add latest tag for 1672 by Jenkins Update from previous latest version(1673) are....
    ------------------------------------------------------------------------
    r1672 | kei.okada@gmail.com | 2011-11-12 21:17:54 +0900 (土, 12 11月 2011) | 1 line
    
    add start in getopt
    ------------------------------------------------------------------------

--- tags/latest/rtmros_common/openhrp3/test/sample-projects.sh
+++ tags/latest/rtmros_common/openhrp3/test/sample-projects.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 
-# install cnee http://blog.livedoor.jp/vine_user/archives/51738792.html, use xnee-3.10.tar.gz
+PROJECT_FILE=$1
 TEST_DIR=`rospack find openhrp3`/test
+
+. `rospack find openhrp3`/test/test-grxui-lib.sh
+
 
 function check-sample-project {
     local filename=$1
@@ -16,44 +19,12 @@ function check-sample-project {
     #
     echo "start $filename" 1>&2
     rosrun openhrp3 grxui.sh $filename > /dev/null &
-    while :; do
-	rosrun openhrp3 check-online-viewer.py
-	if [ $? == 0 ] ; then
-	    break;
-	fi;
-	echo ";; Wait for GRXUI of $filename" 1>&2;
-	sleep 1;
-    done
-    # start simulator
-    cnee --replay --time 5 -fcr -f $TEST_DIR/cnee-grxui-start.xns  1>&2
-    # capture image
-    sleep 5;
-    cnee --replay --time 0 -fcr -f $TEST_DIR/cnee-grxui-return.xns  1>&2
-    cnee --replay --time 0 -fcr -f $TEST_DIR/cnee-grxui-return.xns  1>&2
-    import -window Eclipse\ SDK\  $TEST_DIR/project-`basename $filename .xml`.png  1>&2
-    # done
-    cnee --replay --time 1 -fcr -f $TEST_DIR/cnee-grxui-return.xns  1>&2
-    cnee --replay --time 1 -fcr -f $TEST_DIR/cnee-grxui-return.xns  1>&2
-    cnee --replay --time 1 -fcr -f $TEST_DIR/cnee-grxui-quit.xns    1>&2
-    cnee --replay --time 1 -fcr -f $TEST_DIR/cnee-grxui-return.xns  1>&2
+    wait-grxui
+    start-capture-grxui $TEST_DIR/project-`basename $filename .xml`.png
     # make sure to kill eclipse
     ps $! && kill -KILL $!
 }
 
-PROJECT_FILE=$1
-GETOPT=`getopt -q -l gtest_output: -- "$@"` ; [ $? != 0 ] && exit 1
-eval set -- "$GETOPT"
-while true
-do
-  case $1 in
-  --gtest_output)  TEST_OUTPUT=`echo $2|sed s/^xml\://`     ; shift 2
-	  ;;
-  --)  shift; break
-	  ;;
-  esac
-done
-
-touch $TEST_OUTPUT # for dummy gtest output
 check-sample-project $PROJECT_FILE
 
 
