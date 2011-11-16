2456ecda9b4604222fb90dab35ac60a1165db4fd
rtmrosrobotics.testing@gmail.com <rtmrosrobotics.testing@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-11-16 13:55:04 +0000

    Add latest tag for 1767 by Jenkins Update from previous latest version(1769) are....
    ------------------------------------------------------------------------
    r1767 | kei.okada@gmail.com | 2011-11-16 21:29:50 +0900 (水, 16 11月 2011) | 1 line
    
    update import script
    ------------------------------------------------------------------------

--- tags/latest/rtmros_common/openhrp3/test/sample-projects.py
+++ tags/latest/rtmros_common/openhrp3/test/sample-projects.py
@@ -99,7 +99,7 @@ class TestGrxUIProject(unittest.TestCase):
         self.unmap_window("Time is up")
 
     def capture_eclipse(self):
-        subprocess.call("import -screen -window Eclipse\ SDK\  "+self.capture_filename, shell=True)
+        subprocess.call("import -frame -screen -window Eclipse\ SDK\  "+self.capture_filename, shell=True)
 
     def exit_eclipse(self):
         self.map_window("Time is up")
@@ -108,7 +108,7 @@ class TestGrxUIProject(unittest.TestCase):
             self.wait_for_window("Simulation Finished")
             self.return_window("Simulation Finished")
         subprocess.call("xdotool search --name \"Eclipse SDK\" windowactivate --sync key --clearmodifiers alt+f key --clearmodifiers x", shell=True)
-        subprocess.call("xdotool key --clearmodifiers return", shell=True)
+        subprocess.call("xdotool key --clearmodifiers Return", shell=True)
         # wait 10 seconds?
         i = 0
         while self.proc and self.proc.poll() == None and i < 10:
