24a1de1dc7315622f4d33e784d5618417b643a0e
rtmrosrobotics.testing@gmail.com <rtmrosrobotics.testing@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-11-24 11:48:52 +0000

    Add latest tag for 1886 by Jenkins Update from previous latest version(1868) are....
    ------------------------------------------------------------------------
    r1870 | kei.okada@gmail.com | 2011-11-24 15:23:39 +0900 (木, 24 11月 2011) | 1 line
    
    update rtm-ros-robotics.rosinstall directory
    ------------------------------------------------------------------------
    r1871 | kei.okada@gmail.com | 2011-11-24 15:29:10 +0900 (木, 24 11月 2011) | 1 line
    
    update rtm-ros-robotics.rosinstall directory
    ------------------------------------------------------------------------
    r1872 | kei.okada@gmail.com | 2011-11-24 15:37:08 +0900 (木, 24 11月 2011) | 1 line
    
    update rtm-ros-robotics.rosinstall directory
    ------------------------------------------------------------------------
    r1873 | notheworld@gmail.com | 2011-11-24 16:43:10 +0900 (木, 24 11月 2011) | 1 line
    
    fix yaml file for darwin ;; previous version's darwin.yaml does not satisfy DARWIN.wrl's order of joints
    ------------------------------------------------------------------------
    r1874 | notheworld@gmail.com | 2011-11-24 16:52:42 +0900 (木, 24 11月 2011) | 1 line
    
    define head end-coords for darwin euslisp models
    ------------------------------------------------------------------------
    r1876 | kei.okada@gmail.com | 2011-11-24 18:30:44 +0900 (木, 24 11月 2011) | 1 line
    
    integrate test code into launch file and use rosdoc to generate documents
    ------------------------------------------------------------------------
    r1877 | kei.okada@gmail.com | 2011-11-24 18:50:54 +0900 (木, 24 11月 2011) | 1 line
    
    add launch/falling-box.launch 
    ------------------------------------------------------------------------
    r1878 | kei.okada@gmail.com | 2011-11-24 19:13:19 +0900 (木, 24 11月 2011) | 1 line
    
    add retry=2
    ------------------------------------------------------------------------
    r1879 | kei.okada@gmail.com | 2011-11-24 19:26:51 +0900 (木, 24 11月 2011) | 1 line
    
    ad more descriptions in launch file
    ------------------------------------------------------------------------
    r1880 | kei.okada@gmail.com | 2011-11-24 19:29:05 +0900 (木, 24 11月 2011) | 1 line
    
    add rosdoc.yaml
    ------------------------------------------------------------------------
    r1881 | kei.okada@gmail.com | 2011-11-24 19:52:41 +0900 (木, 24 11月 2011) | 1 line
    
    wait until corba server comes up
    ------------------------------------------------------------------------
    r1882 | kei.okada@gmail.com | 2011-11-24 19:53:18 +0900 (木, 24 11月 2011) | 1 line
    
    wait until corba server comes up
    ------------------------------------------------------------------------
    r1883 | kei.okada@gmail.com | 2011-11-24 20:14:39 +0900 (木, 24 11月 2011) | 1 line
    
    fix image width from 800->600
    ------------------------------------------------------------------------
    r1884 | kei.okada@gmail.com | 2011-11-24 20:18:14 +0900 (木, 24 11月 2011) | 1 line
    
    remove test directory use rosdoc and launchfiles
    ------------------------------------------------------------------------
    r1885 | notheworld@gmail.com | 2011-11-24 20:20:18 +0900 (木, 24 11月 2011) | 1 line
    
    fix for darwin's pelv yaw joint
    ------------------------------------------------------------------------
    r1886 | kei.okada@gmail.com | 2011-11-24 20:30:10 +0900 (木, 24 11月 2011) | 1 line
    
    remove test directory use rosdoc and launchfiles
    ------------------------------------------------------------------------

--- tags/latest/rtmros_common/jenkins_build.sh
+++ tags/latest/rtmros_common/jenkins_build.sh
@@ -9,8 +9,8 @@ find $WORKSPACE -maxdepth 1 -name "rtm-ros-robotics-*" -a ! -name "rtm-ros-robot
 # rosinstall
 rosinstall --continue-on-error $WORKSPACE/rtm-ros-robotics-$BUILD_NUMBER /opt/ros/electric http://rtm-ros-robotics.googlecode.com/svn/trunk/agentsystem_ros_tutorials/rtm-ros-robotics.rosinstall || rosinstall $WORKSPACE/rtm-ros-robotics-$BUILD_NUMBER
 # copy jenkins source
-rm -fr rtm-ros-robotics-$BUILD_NUMBER/rtmros_common/
-cp -r rtm-ros-robotics/rtmros_common rtm-ros-robotics-$BUILD_NUMBER/rtmros_common
+rm -fr rtm-ros-robotics-$BUILD_NUMBER/rtm-ros-robotics/rtmros_common/
+cp -r rtm-ros-robotics/rtmros_common rtm-ros-robotics-$BUILD_NUMBER/rtm-ros-robotics/rtmros_common
 # set ROS_HOME under workspace so that we can check from web interface
 echo "export ROS_HOME=$WORKSPACE/.ros" >> rtm-ros-robotics-$BUILD_NUMBER/setup.sh
 # source
