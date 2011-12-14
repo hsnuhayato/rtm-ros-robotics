725915a3b975bf25fdf5149093c5a83350a9b446
m117120@gmail.com <m117120@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-12-14 14:25:41 +0000

    debuged

--- agentsystem_hironx_samples/semi/init.py
+++ agentsystem_hironx_samples/semi/init.py
@@ -20,7 +20,7 @@ detectpose = [0.0, 0.0, 1.1,
               -0.42, -0.42, -2.31, -0.69, 1.20, -0.22,
               0.64, -0.63, -0.62, 0.63,
               0.62, -0.63, -0.64, 0.64]
-detectpose2 = [0.0, 0.0, 1.1,
+orordetectpose2 = [0.0, 0.0, 1.1,
               0.70, -0.38, -2.23, 0.52, 1.16, -0.23,
               -0.42, -0.42, -2.31, -0.69, 1.20, -0.22,
               0.64, -0.63, -0.62, 0.63,
@@ -41,8 +41,10 @@ manip=orrobot.SetActiveManipulator("leftarm_torso")
 self = cubeassembly.CubeAssembly(orrobot)
 self.CreateBlocks(generategrasps=True)
 
-Tworld_goal = eye(4)
-Tworld_goal[0:3,3] = [0.4,0,0] # 編集が必要です
+Tworld_goal=eye(4)
+Tworld_goal[0][3]=0
+Tworld_goal[1][3]=-0.07
+Tworld_goal[2][3]=0.1
 
 basemanip = interfaces.BaseManipulation(orrobot)
 taskmanip=interfaces.TaskManipulation(orrobot)
