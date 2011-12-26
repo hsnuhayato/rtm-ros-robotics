21802fd5790d840ea2454559569b04379a0b47ea
rosen.diankov@gmail.com <rosen.diankov@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2011-12-26 11:20:37 +0000

    b5

--- rst/conf.py
+++ rst/conf.py
@@ -172,7 +172,7 @@ htmlhelp_basename = 'rtm_ros_roboticsdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-latex_paper_size = 'a4'
+latex_paper_size = 'b5'
 
 # The font size ('10pt', '11pt' or '12pt').
 latex_font_size = '10pt'
