da82fdc4101c25a3624f22d30eac2e2036a4f0a3
gm130s@gmail.com <gm130s@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-12-03 11:26:35 +0000

    (hrpsys) tagging 315.1.2 (2nd trial)

--- rtmros_gazebo/hrpsys_gazebo_atlas/scripts/hrpsys_servo_off.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rosservice call /RobotHardwareServiceROSBridge/servo '{name: "all", ss: 1}'
-sleep 1
-rosservice call /RobotHardwareServiceROSBridge/power '{name: "all", ss: 1}'
--- rtmros_gazebo/hrpsys_gazebo_atlas/scripts/hrpsys_servo_on.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rosservice call /RobotHardwareServiceROSBridge/power '{name: "all", ss: 0}'
-sleep 1
-rosservice call /StateHolderServiceROSBridge/goActual
-sleep 2
-rosservice call /RobotHardwareServiceROSBridge/servo '{name: "all", ss: 0}'
