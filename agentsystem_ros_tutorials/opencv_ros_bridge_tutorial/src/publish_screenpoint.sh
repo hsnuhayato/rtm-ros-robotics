1cebaf6948753c17be864cddd6ebff464e51b026
gm130s@gmail.com <gm130s@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-01-14 08:47:47 +0000

    "tagging rtmros_hironx-1.0.8"

--- agentsystem_ros_tutorials/opencv_ros_bridge_tutorial/src/publish_screenpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# http://www.ros.org/wiki/rostopic#rostopic_pub
-X=${1:-100}
-Y=${2:-200}
-rostopic pub -1 /image_painted/screenpoint geometry_msgs/PointStamped "{point: {x: $X, y: $Y}}"
-
--- rtmros_gazebo/hrpsys_gazebo_tutorials/robot_models/HRP4C/install_robot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$(rospack find hrpsys_gazebo_tutorials)/robot_models/install_robot_common.sh HRP4C
